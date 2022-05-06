5f74961d10eb5950d71c9f449ae2079069a9820d
wdpfeffer <wdpfeffer@gmail.com>
2022-05-06 10:01:02 -0600

    working pushbutton test

--- chamber_control/chamber_control.ino
+++ chamber_control/chamber_control.ino
@@ -5,35 +5,27 @@
 #define temp 0
 #define hum 1
 #define sensorInterval 10000 //read every 10
-#define pbInterval 50 //pushbutton interval
+#define debounceDelay 50 //pushbutton interval
 
 
-
-int lightsButton;
-int lastLightsButton;
+byte lightsButtonReading;
+byte lightsButtonState;
+byte lastLightsButtonState = HIGH;
 
 unsigned long sensorCycleStart;
 unsigned long sensorCycleCurrent;
-unsigned long lightPush;
+unsigned long lastDebounceTimeLights;
 
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(swLight, INPUT_PULLDOWN);
+  pinMode(swLight, INPUT);
   
-  pinMode(lightLED, OUTPUT);
+  pinMode(lightLED, OUTPUT);uu
   pinMode(PC13, OUTPUT);   //board status
   
-  //setup serial for debugging
-  Serial.begin(9600);
-
- 
-  //Set button stats
-  lastLightsButton = LOW;
-
   sensorCycleStart = millis();
-  lightPush = 0;
-
+ 
 }
 
 void loop() {
@@ -57,14 +49,35 @@ void loop() {
     }
   }
 
-  if (digitalRead(swLight)==HIGH && millis()-lightPush > pbInterval){
-    if (digitalRead(lightLED)==HIGH){
-      digitalWrite(lightLED, LOW);
-    }else{
-      digitalWrite(lightLED, HIGH);
-    }
-    lightPush=millis();   
+  //Evaluate button pusheu
+
+  lightsButtonReading = digitalRead(swLight);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (lightsButtonReading != lastLightsButtonState) {
+    // reset the debouncing timer
+    lastDebounceTimeLights = millis();
   }
+
+  if ((millis() - lastDebounceTimeLights) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (lightsButtonReading != lightsButtonState) {
+      lightsButtonState = lightsButtonReading;
+
+      //toggle the LED
+      
+    }
+  }
+
+    // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastLightsButtonState = lightsButtonReading;
 
 //  //Check for button pushes
 //  lightsButton = digitalRead(swLight);
